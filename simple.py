c5ab5ef9f37c74bbb26df69adb912e9d4c650d76
legasri1208 <legasri1208@gmail.com>
2025-07-16 12:49:50 +0530

    Add files via upload

---
+++ simple.py
@@ -0,0 +1,9 @@
+for num in range (1 , 20):
+    if num % 2 == 0 and num % 4 == 0:
+        print ("yesno")
+    elif num %2 == 0:
+        print ("yes")
+    elif num %4 == 0:
+        print ("no")
+    else:
+      print(num)
